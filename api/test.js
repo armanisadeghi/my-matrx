ddfda20f97318b8339f03a857ca3da7697122c83
armanisadeghi <132974515+armanisadeghi@users.noreply.github.com>
2025-09-28 08:55:52 -0700

    Improve CORS headers: add more methods, headers, and set credentials to false

--- api/test.js
+++ api/test.js
@@ -15,9 +15,9 @@ export default function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
   }
   
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+  res.setHeader('Access-Control-Allow-Credentials', 'false');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
